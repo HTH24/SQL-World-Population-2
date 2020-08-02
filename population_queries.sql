d3736199c0df3e9662e8b3ae853f29075b54308a
HTH24 <64564685+HTH24@users.noreply.github.com>
2020-08-01 20:58:18 -0400

    Add files via upload

---
+++ population_queries.sql
@@ -0,0 +1,27 @@
+-- How many entries in the database are from Africa?
+-- 56
+
+
+-- What was the total population of Oceania in 2005?
+-- 32.66417
+
+
+-- What is the average population of countries in South America in 2003?
+-- 25.89
+
+
+-- What country had the smallest population in 2007?
+-- Niue, 0.00216
+
+
+-- What is the average population of Poland during the time period covered by this dataset?
+-- 38.56
+
+
+-- How many countries have the word "The" in their name?
+-- 4. The Bahamas, The Gambia, Netherlands, -- if this one counts, and Netherlands Antilles
+
+
+-- What was the total population of each continent in 2010?
+-- Oceania: 34.95696; South America: 396.58235; North America: 539.79456; Europe: 723; Africa: 1015.47846; Asia: 4133.
+
